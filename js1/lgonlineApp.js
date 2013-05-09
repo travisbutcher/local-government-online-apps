14c42e92220490b875c186d0c98148b19d84cd9a
john gravois <jgravois@esri.com>
2013-05-08 17:03:17 -0700

    bing maps null spatial reference fix
    for some reason, a webmap which uses a Bing Maps basemap returns a null
    spatialReference.  added a bit of a hacky approach to set it as web
    mercator manually in this situation.

--- js1/lgonlineApp.js
+++ js1/lgonlineApp.js
@@ -2112,6 +2112,13 @@ define("js/lgonlineApp", ["dijit", "dijit/registry", "dojo/dom-construct", "dojo
             utils.createMap(this.mapId, this.rootDiv, options).then(
                 function (response) {
                     pThis.mapInfo = response;
+					
+					//for some reason if the webmap uses a bing map basemap the response doesn't have a spatialReference defined.
+					//this is a bit of a hack to set it manually
+					if (!response.map.spatialReference) {
+					    pThis.mapInfo.map.spatialReference = new esri.SpatialReference({wkid:102100});
+					}
+					
                     //pThis.listeners.push(
                     //    dojo.connect(pThis.mapInfo.map, "onUnload", function () {  // release event listeners upon unload
                     //        // http://help.arcgis.com/en/webapi/javascript/arcgis/help/jshelp/inside_events.htm
