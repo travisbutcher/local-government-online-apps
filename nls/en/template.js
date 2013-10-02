a07f9e9b27396b24a35e2a20887a3a7c099d5786
MikeTschudi <mtschudi@esri.com>
2013-10-02 11:25:05 -0700

    Added new tooltips to the English-specific file

--- nls/en/template.js
+++ nls/en/template.js
@@ -21,6 +21,8 @@ define(
             search: "Search",
             locate: "Current location",
             markup: "Submit correction",
+            collect: "Add content to map",
+            filter: "Filter map layers",
             basemap: "Switch basemap",
             share: "Share",
             shareViaEmail: "Share via email",
